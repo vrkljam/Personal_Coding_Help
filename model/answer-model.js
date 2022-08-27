e7f5f11d921953c56f1a0357d6ff5c2793fc9eb4
vrkljam <vrkljam@earthlink.net>
2022-08-27 15:12:05 -0600

    updates to pkg json

--- model/answer-model.js
+++ model/answer-model.js
@@ -3,14 +3,13 @@ const mongoose =require('../db/connnection');
 
 const HowtoSchema = new mongoose.Schema(
         {
-        title: {type:String, 
-            required:true},
-            description:String,
-            url:String,
-            dateAdded:Date,
-            img:String,
-            category:[String],
-            codeblock:String
+                title: {type:String, required:true},
+                description:String,
+                url:String,
+                dateAdded:Date,
+                img:String,
+                category:[String],
+                codeblock:String
         },
         
         {timestamps:true}
@@ -18,11 +17,3 @@ const HowtoSchema = new mongoose.Schema(
         
 const Howto = mongoose.model('Howto', HowtoSchema);
 module.exports=Howto;
-        
-        
-        // category:[CategorySchema]  
-        
-        // img:{type:String,default:null}
-        // const CategorySchema = new mongoose.Schema ({
-        //     name:String
-        // })
